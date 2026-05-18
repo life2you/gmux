3c3c68430e0af563f69e1eb0898850064e6a6a01
life2you <henry@henglin.com>
2026-05-18 13:50:54 +0800

    chore: refresh formula for v0.1.8

--- packaging/homebrew-tap/Formula/gmux.rb
+++ packaging/homebrew-tap/Formula/gmux.rb
@@ -1,18 +1,18 @@
 class Gmux < Formula
   desc "Terminal Git workflow tool for multi-env branch sync and GitLab MR automation"
   homepage "https://github.com/life2you/gmux"
-  version "0.1.7"
+  version "0.1.8"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/life2you/gmux/releases/download/v0.1.7/gmux-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_ARM64_SHA256"
+      url "https://github.com/life2you/gmux/releases/download/v0.1.8/gmux-aarch64-apple-darwin.tar.gz"
+      sha256 "030573c7e16c80c3ceed16e249003993e7a9ad6cfa8d01cf1ae4ca8eb7e287d1"
     end
 
     on_intel do
-      url "https://github.com/life2you/gmux/releases/download/v0.1.7/gmux-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_X64_SHA256"
+      url "https://github.com/life2you/gmux/releases/download/v0.1.8/gmux-x86_64-apple-darwin.tar.gz"
+      sha256 "fac47b520cad2e217035826551ef4274c9da52002c14f71416a98d313b46b4fb"
     end
   end
 
